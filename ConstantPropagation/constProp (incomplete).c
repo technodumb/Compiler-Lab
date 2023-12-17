a3d747ca35d803bd9673952f4fca89ab1a15c5f4
Alnas Kabeer <alnaskabeer@gmail.com>
2023-12-17 22:23:51 +0530

    made constant propagation better

--- ConstantPropagation/constProp (incomplete).c
+++ ConstantPropagation/constProp (incomplete).c
@@ -4,9 +4,36 @@
 #include<ctype.h>
 #include<stdbool.h>
 
+
+char keywords[][20] = {"exit", "for", "while", "return", "if", "else", "printf", "main"};
+char dtypes[][20] = {"void", "int", "char", "float"};
+int symtabType[20], declarationType;
+char symtab[20][20];
+double symtabval[20];
+char word[20], idStack[20][20], opStack[20];
+int wordptr=0, idStackTop=0, opStackTop=0, nSymtab=0;
+bool isDeclaration = false, isRHS = false;
+
+float getSymbolValue(char *symbol){
+    printf("symbol: %s\n", symbol);
+    float value;
+    if(isdigit(symbol[0]))
+        value = atof(symbol);
+    else {
+
+        for(int i=0; i<nSymtab; i++){
+            if(strcmp(symbol, symtab[i])==0){
+                printf("Present in symtab: %s\n", symbol);
+                value = symtabval[i];
+                break;
+            }
+        }
+    }
+    printf("value: %lf\n", value);
+    return value;
+}
+
 void main() {
-    char keywords[][20] = {"void", "int", "char", "exit", "for", "while", "return", "if", "else", "printf", "main"};
-    char dtypes[][20] = {"int", "char", "float"};
 
     FILE *finp, *fcon, *fout;
     finp = fopen("input.txt", "r");
@@ -18,12 +45,11 @@ void main() {
         exit(0);
     }
 
-    char word[20], idStack[20][20], opStack[20];
-    int wordptr=0, idStackTop=0, opStackTop=0;
+    while(true) {
+        char c = fgetc(finp);
+        if(c == EOF) break;
 
-    char c = fgetc(finp);
-
-    while(!feof(finp)) {
+        // ignoring #includes
         if(c == '#'){
             fprintf(fout, "#");
             while((c = fgetc(finp)) != '\n'){
@@ -31,41 +57,149 @@ void main() {
             }
             fprintf(fout, "\n");
         }
-        if(c == '"'){
+        // ignoring strings
+        else if(c == '"'){
             fprintf(fout, "\"");
             while((c = fgetc(finp)) != '"'){
                 fprintf(fout, "%c", c);
             }
             fprintf(fout, "\"");
         }
-        else if(isdigit(c)){
-            word[wordptr++] = c;
-            while(isdigit(c=fgetc(finp))){
-                word[wordptr++] = c;
-            }
-            if(isalpha(c)){
-                printf("Invalid lexeme.\n");
-                exit(0);
-            }
-            fseek(finp, -1, SEEK_CUR);
-            word[wordptr++] = 0;
-            strcpy(idStack[idStackTop++], word);
-            wordptr=0;
+        else if(c == ' '){
+            fprintf(fout, " ");
+            continue;
         }
-        else if(isalpha(c)){
-            word[wordptr++] = c;
-            while(isalnum(c=fgetc(finp))){
+        else if(isalnum(c)){
+
+            if(isdigit(c)){
                 word[wordptr++] = c;
+                while(isdigit(c=fgetc(finp)) || c=='.'){
+                    word[wordptr++] = c;
+                }
+                if(isalpha(c)){
+                    printf("Invalid lexeme.\n");
+                    exit(0);
+                }
+                fseek(finp, -1, SEEK_CUR);
+                word[wordptr++] = 0;
+                strcpy(idStack[idStackTop++], word);
+                wordptr=0;
             }
-            fseek(finp, -1, SEEK_CUR);
-            word[wordptr++] = 0;
-            strcpy(idStack[idStackTop++], word);
-            wordptr=0;
+
+            else if(isalpha(c)){
+                word[wordptr++] = c;
+                while(isalnum(c=fgetc(finp))){
+                    word[wordptr++] = c;
+                }
+                fseek(finp, -1, SEEK_CUR);
+                word[wordptr++] = 0;
+
+                for(int i=0; i<8; i++){
+                    if(strcmp(word, keywords[i])==0){
+                        fprintf(fout, "%s", word);
+                        wordptr=0;
+                        break;
+                    }
+                }
+
+                if(!wordptr) continue;
+
+                for(int i=0; i<4; i++){
+                    if(strcmp(word, dtypes[i])==0){
+                        fprintf(fout, "%s", word);
+                        wordptr=0;
+                        isDeclaration = true;
+                        declarationType = i;
+                        break;
+                    }
+                }
+
+                if(!wordptr) continue;
+
+                if(isDeclaration){
+                    strcpy(symtab[nSymtab], word);
+                    symtabType[nSymtab] = declarationType;
+                    symtabval[nSymtab++] = 0;
+                    fprintf(fout, "%s", word);
+                }
+                strcpy(idStack[idStackTop++], word);
+                wordptr=0;
+            }
+            
+            if(opStack[opStackTop-1] == '*' || opStack[opStackTop-1] == '/'){
+                float oper2 = getSymbolValue(idStack[--idStackTop]);
+                float oper1 = getSymbolValue(idStack[--idStackTop]);
+                float res;
+                if(opStack[--opStackTop] == '*'){
+                    res = oper1 * oper2;
+                }
+                else {
+                    res = oper1 / oper2;
+                }
+                sprintf(idStack[idStackTop++], "%lf", res);
+            }
+        }
+
+
+        else if(c == '+' || c == '-' || c == '*' || c == '/'){
+            // if(isRHS){
+            opStack[opStackTop++] = c;
+        }
+
+        else if(c=='=') {
+            if(isDeclaration){
+                isDeclaration = false;
+            }
+            else {
+                fprintf(fout, "%s", idStack[0]);
+            }
+            opStack[opStackTop++] = c;
+            fprintf(fout, "=");
+        }
+
+        else if(c == ',' || c==';'){            
+            if(opStack[0] == '=' && opStackTop > 0){
+                float oper1, oper2;
+                oper1 = getSymbolValue(idStack[1]); 
+
+                for(int i=1; i<opStackTop; i++){
+                    oper2 = getSymbolValue(idStack[i+1]);
+                    float res;
+                    if(opStack[i] == '+'){
+                        res = oper1 + oper2;
+                    }
+                    else {
+                        res = oper1 - oper2;
+                    }
+                    oper1 = res;
+                }
+
+                for(int i=0; i<nSymtab; i++){
+                    if(strcmp(idStack[0], symtab[i])==0){
+                        symtabval[i] = oper1;
+                        if(symtabType[i]==1){
+                            fprintf(fout, "%d", (int) oper1);
+                        }
+                        else if(symtabType[i]==3){
+                            fprintf(fout, "%lf", oper1);
+                        }
+                        break;
+                    }
+                }
+
+            }
+            if(c == ';')
+                isDeclaration = false;
+            if(c== ',')
+                isDeclaration = true;
+            opStackTop = 0;
+            idStackTop = 0;
+
+            fprintf(fout, "%c", c);
         }
         else{ 
             fprintf(fout, "%c", c);
             printf("%c", c);
         }
-        c = fgetc(finp);   
     }
 }
